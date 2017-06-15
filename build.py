9d131a209ff497c4b7dce0e40089431c8ddd7341
sankbhatt <sankbhatt@users.noreply.github.com>
2017-06-15 19:10:21 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,10 @@
 def solution(a_list):
     """Enter Code Here"""
+    #lst = a_list.copy()
+    #x = lst.remove(min(lst))
+    #return min(a_list)
+    return sorted(a_list)[1]
+
+
+d = [1, 2, -8, -2, 0]
+print solution(d)
