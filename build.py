b7fc8f8c79c4cd9f8cd0a4d3915a7719bb202981
bhattbhavesh91 <bhattbhavesh91@gmail.com>
2017-06-16 11:58:24 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,16 @@
+'''
+Write a Python program to find the second smallest number in a list.
+Define a function that takes a list of numbers as a parameter and returns the second smallest number in the list.
+Example:
+
+Input
+
+[1, 2, -8, -2, 0]
+
+Output
+
+-2
+'''
 def solution(a_list):
-    """Enter Code Here"""
+    x = [x for x in sorted(a_list) if x!= min(a_list)]
+    return x[0]
