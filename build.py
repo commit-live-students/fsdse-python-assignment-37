f110746d8ec8ac54382ecc50e3537ce49d6450f6
pramodbhalerao <pramodbhalerao_01@yahoo.com>
2017-06-20 19:16:03 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,7 @@
 def solution(a_list):
-    """Enter Code Here"""
+    first_min = min(int(s) for s in a_list)
+    a_list.remove(first_min)
+    second_min = min(int(s) for s in a_list)
+    return second_min
+
+solution([1, 2, -8, -2, 0])
