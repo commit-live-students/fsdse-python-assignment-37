932c9e92ddf138449ba8b096ac18d0094191a6c5
ramkrishan-sahu <ram04190@gmail.com>
2017-06-21 13:53:41 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
-def solution(a_list):
-    """Enter Code Here"""
+lis = [1, 2, -8, -2, 0]
+
+def solution(lis):
+    lis2 = lis.sort()
+    print (lis)
+    print (lis[1])
+    return lis[1]
+
+solution(lis)
