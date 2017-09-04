46c8c7770b0929d495247a72693a73b62b5528c3
darshind <doshi.darshin@gmail.com>
2017-09-04 14:41:54 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,9 @@
 def solution(a_list):
-    """Enter Code Here"""
+    m1,m2 = float('inf'), float('inf')
+    for x in a_list:
+        if x <= m1:
+            m1, m2 = x, m1
+        elif x < m2:
+            m2 = x
+    return m2
+print solution([1, 2, -8, -2, 0])
